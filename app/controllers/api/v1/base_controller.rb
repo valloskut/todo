d765d0ca531c0efbd60350a5ce8b1f3feb63fad9
Valeriy Loskutnikov <valloskut@gmail.com>
2014-04-29 02:11:48 -0400

    added rescue from exception to API controller

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::BaseController < ApplicationController
-  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
   respond_to :json
+
+  rescue_from Exception, with: :render_error
+
+  protected
+
+  def render_error(exception)
+    render json: {error: exception.to_s}, status: :bad_request
+  end
 end
